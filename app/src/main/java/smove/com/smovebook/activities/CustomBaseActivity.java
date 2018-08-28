5c40330f4952a05bd5bca188b90214d5368c8ee5
Manuv <Manuve@sg.ibm.com>
2018-08-28 15:54:40 +0800

    Date and time picker added

---
+++ app/src/main/java/smove/com/smovebook/activities/CustomBaseActivity.java
@@ -0,0 +1,12 @@
+package smove.com.smovebook.activities;
+
+import android.support.v7.app.AppCompatActivity;
+
+
+/**
+ * Created by Manuramv on 8/28/2018.
+ */
+
+public class CustomBaseActivity extends AppCompatActivity {
+
+}
